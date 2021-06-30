8f313e0b0c5cff227ea0bb5c91027351301f5055
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-06-30 14:48:20 +0530

    Changed the hyperparameter tuning

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ tf.config.threading.set_intra_op_parallelism_threads(2)
 tf.config.threading.set_inter_op_parallelism_threads(2)
 
 mb = 32  # Size of the minibatch
-split = 4
+split = 5
 # split = int(sys.argv[1])  # test split
 
 dataset = scipy.io.loadmat('./Dataset/qm7.mat')  # Using QM7 dataset
@@ -53,7 +53,9 @@ def process_matrix(x):
     except:
         print(x)
         quit()
-    x = x.flatten()
+    # As the matrix is symmetric, we take only the lower diagonal
+    x = x[np.triu_indices_from(x)]
+    pdb.set_trace()
     return x
 
 
@@ -65,68 +67,63 @@ X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
 
-# Start the learning rate from 10^(-4) and going on till 10^(-6)
-# lr = lr0/(1+kt) where lr, k are hyperparameters and t is the iteration number
-epochs = 20000
-lr0 = 10**(-4)
-decay_rate = 99 / 5000 
-momentum = 0.8
-sgd = keras.optimizers.SGD(learning_rate=lr0, momentum=momentum, decay=decay_rate, nesterov=False)
-
-
 def build_model(hp):
     # Define the NN model
     model = Sequential()
 
     # Add an input layer 
-    model.add(keras.layers.Flatten(input_shape=(529,)))
+    model.add(keras.layers.Flatten(input_shape=(276,)))
 
-    # Add one hidden layer with nodes between 32 and 2048
-    hp_units = hp.Int('unit1', min_value=32, max_value=2048, step=5)
-    hp_activation = hp.Choice('activation1',
+    # Add one hidden layer with nodes between 32 and 256
+    hp_units = hp.Int('unit1', min_value=32, max_value=256, step=32)
+    hp_activation = hp.Choice('activation',
                               values=['relu', 'tanh', 'sigmoid'],
                               default='relu'
                               )
     model.add(keras.layers.Dense(units=hp_units, activation=hp_activation))
 
-    # Add one hidden layer with nodes between 32 and 2048
-    hp_units = hp.Int('unit2', min_value=32, max_value=2048, step=5)
-    hp_activation = hp.Choice('activation2',
-                              values=['relu', 'tanh', 'sigmoid', 'softmax', 'softplus'],
-                              default='relu'
-                              )
+    # Add one hidden layer with nodes between 32 and 256
+    hp_units = hp.Int('unit2', min_value=32, max_value=256, step=32)
+
     model.add(keras.layers.Dense(units=hp_units, activation=hp_activation))
 
-    # Add one hidden layer with nodes between 32 and 2048
-    hp_units = hp.Int('unit3', min_value=32, max_value=2048, step=5)
-    hp_activation = hp.Choice('activation3',
-                              values=['relu', 'tanh', 'sigmoid'],
-                              default='relu'
-                              )
+    # Add one hidden layer with nodes between 32 and 256
+    hp_units = hp.Int('unit3', min_value=32, max_value=256, step=32)  # Step of 32
+
     model.add(keras.layers.Dense(units=hp_units, activation=hp_activation))
 
     # Add an output layer 
     model.add(Dense(1, activation='linear'))
 
     model.compile(loss='mse',
-                  optimizer='sgd',
+                  optimizer='adam',
                   metrics=['mae'])
 
     return model
 
 
+class MyTuner(BayesianOptimization):
+    def run_trial(self, trial, *args, **kwargs):
+        '''
+        Overriding the run_trial method to change the batchsize and the number of epochs
+        '''
+        kwargs['batch_size'] = trial.hyperparameters.Int('batch_size', 32, 64, step=32)
+        kwargs['epochs'] = trial.hyperparameters.Int('epochs', 20, 100)
+        super(MyTuner, self).run_trial(trial, *args, **kwargs)
+
+
 # Define a tuner to tune the hyperparameters
-tuner = BayesianOptimization(
+tuner = MyTuner(
     build_model,
     objective='val_mae',
-    max_trials=400,
-    project_name='AE_prediction_3',
+    max_trials=500,
+    project_name='AE_prediction_final',
     directory='/Volumes/Seagate Por/Thesis/bo/')
 
-# If the mae does not change by at least 1 units over a span of 100 epochs, stop the training.
-stop_early = tf.keras.callbacks.EarlyStopping(monitor='mae', min_delta=2, patience=10)
+# If the mae does not improve over a span of 30 epochs, stop the training.
+stop_early = tf.keras.callbacks.EarlyStopping(monitor='mae', patience=30)
 
-tuner.search(X_train, y_train, epochs=50, validation_split=0.33, callbacks=[stop_early])
+tuner.search(X_train, y_train, validation_split=0.33, shuffle=True, use_multiprocessing=True, callbacks=[stop_early])
 best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
 pdb.set_trace()
@@ -135,10 +132,25 @@ pdb.set_trace()
 model = tuner.hypermodel.build(best_hps)
 
 # If the val_mae does not change by at least 2 units over a span of 100 epochs, stop the training.
-stop_early = tf.keras.callbacks.EarlyStopping(monitor='mae', min_delta=0.01, patience=500)
+stop_early = tf.keras.callbacks.EarlyStopping(monitor='mae', patience=500)
+
+# Change the learning rate after each epoch based on the relation: lr=lr0/(1+kt)
+
+
+def adapt_learning_rate(epoch):
+    lr0 = 10**(-4)
+    epochs = 20000
+    decay_rate = 99 / epochs 
+    lr = lr0 / (1 + decay_rate * epoch)
+    return lr
+
+
+epochs = 20000
+my_lr_scheduler = keras.callbacks.LearningRateScheduler(adapt_learning_rate)
+
 
 # Train the model
-fitted = model.fit(X_train, y_train, epochs=epochs, validation_split=0.33, batch_size=mb, verbose=1, callbacks=[stop_early])
+fitted = model.fit(X_train, y_train, epochs=epochs, shuffle=True, use_multiprocessing=True, batch_size=mb, verbose=1, callbacks=[stop_early, my_lr_scheduler])
 
 pdb.set_trace()
 
@@ -146,8 +158,10 @@ pdb.set_trace()
 predicted = model.predict(X_test)
 
 print(f"""
-The hyperparameter search is complete. The optimal number of units in the first densely-connected
-layer is {best_hps.get('unit1')} and the 2nd layer is {best_hps.get('unit2')}
+The hyperparameter search is complete. 
+{best_hps.get('unit1')} --- {best_hps.get('activation1')} 
+{best_hps.get('unit2')} --- {best_hps.get('activation2')} 
+{best_hps.get('unit3')} --- {best_hps.get('activation3')} 
 """)
 print('SMAE on the test set is {:}'.format(mean_squared_error(predicted, y_test)))
 print('MAE on the test set is {:}'.format(mean_absolute_error(predicted, y_test)))
